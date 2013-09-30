9aceb27ed05b84c6532e8c93b1ab9a8dbd398589
Tim Proffitt <tim.proffitt@gmail.com>
2013-09-29 22:06:59 -0500

    created proto app

---
+++ proto/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Proto::Application.config.secret_key_base = '6970685dae0a1b94f88f53f7aa09fb67e4915b66f709ca9742b1c23fc2b6bf007830a4e71dc7aaade9be0107825552a113ef5779c756ff12151e11b77790bfd0'
